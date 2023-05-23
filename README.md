75adbed0f74e9a6bf0b63c88144a59382f34f8f6
Mocanu Marian <mocanu.marian@agilefreaks.com>
2023-05-23 14:07:18 +0300

    Migrate script to Python

--- README.md
+++ README.md
@@ -1,23 +1,59 @@
-# App shortcut maker
+## Desktop Shortcut Manager
 
-If you have AppImage packages or tools with a different extension, that you'd like to access more easily this might be the solution for you. This small script allows you to add those tools directly to your applications menu. This way, you can access your costum apps quickly and effortlessly.
+The Desktop Shortcut Manager is a command-line tool written in Python for managing `.desktop` files in the `/usr/share/applications/` directory. It provides options to list, edit, create, and remove `.desktop` files.
 
-## Usage
+### Prerequisites
 
-To use this script, follow these steps:
+- This tool requires administrative privileges to access and modify files in the `/usr/share/applications/` directory. Make sure to run the script with administrative privileges (e.g., using `sudo`).
 
-1. Clone this repository to your local machine.
-2. Open a terminal window and navigate to the directory where the script is located.
-3. Make the script executable with the command: `chmod +x shortcut-maker.sh`.
-4. Run the script with sudo permissions, using the command: `sudo ./shortcut-maker.sh`.
-5. Follow the prompts to enter the required information for the .desktop file.
-6. Review the resulting file and confirm that it is correct.
-7. If confirmed, the file will be created in the `/usr/share/applications directory`.
+### Usage
 
-## Contributing
+1. Download the Python script, `shortcut-manager.py`, to your local machine.
 
-If you find any issues with this script or have ideas for improvements, please feel free to create an issue or submit a pull request.
+2. Open a terminal and navigate to the directory where the `shortcut-manager.py` script is located.
 
-## License
+3. Run the following command to start using the Desktop Shortcut Manager:
 
-This script is released under the MIT License.
+   ```bash
+   python shortcut-manager.py
+   ```
+
+4. The script will display a menu with the following options:
+
+   - **1. List .desktop files:** List all `.desktop` files in the `/usr/share/applications/` directory. You can filter the files by entering a keyword.
+
+   - **2. Edit a .desktop file:** List `.desktop` files and select a file to edit. You can filter the files by entering a keyword.
+
+   - **3. Create a new .desktop file:** Create a new `.desktop` file. You will be prompted to enter the file name, and the script will create the file with the provided name.
+
+   - **4. Remove a .desktop file:** List `.desktop` files and select a file to remove. You can filter the files by entering a keyword.
+
+   - **5. Exit:** Exit the script.
+
+5. Follow the on-screen instructions and enter the appropriate options or values based on your desired action.
+
+6. After completing an operation, the script will display the result or any error messages.
+
+### Examples
+
+#### List files
+
+To list all `.desktop` files, choose option `1` from the menu. You can also enter a keyword to filter the files based on the file name.
+
+#### Edit file
+
+To edit a specific `.desktop` file, choose option `2` from the menu. The script will display the list of `.desktop` files, and you can enter the corresponding file number to edit. You can also enter a keyword to filter the files.
+
+#### Create a new file
+
+To create a new `.desktop` file, choose option `3` from the menu. You will be prompted to enter the file name (without the extension). The script will create the file with the provided name.
+
+#### Remove file
+
+To remove a specific `.desktop` file, choose option `4` from the menu. The script will display the list of `.desktop` files, and you can enter the corresponding file number to remove. You can also enter a keyword to filter the files.
+
+### Notes
+
+- Make sure to back up important files before editing or removing them, as the changes are irreversible.
+
+- If you encounter any issues or errors related to file permissions, make sure to run the script with administrative privileges (e.g., using `sudo`).
---
+++ shortcut-maker.py
@@ -0,0 +1,133 @@
+import os
+import sys
+
+DESKTOP_DIR = "/usr/share/applications/"
+
+def list_desktop_files(keyword=None):
+    print("Available .desktop files:")
+    desktop_files = [f for f in os.listdir(DESKTOP_DIR) if f.endswith(".desktop")]
+    desktop_files.sort()
+    filtered_files = []
+    for i, file in enumerate(desktop_files):
+        if not keyword or keyword.lower() in file.lower():
+            filtered_files.append(file)
+            print(f"{len(filtered_files)}. {file}")
+    if not filtered_files:
+        print("No matching files found.")
+    print()
+
+def edit_desktop_file(file_name):
+    file_path = os.path.join(DESKTOP_DIR, file_name)
+    if not os.path.exists(file_path):
+        print(f"File {file_name} does not exist.")
+        return
+    print(f"Editing file: {file_name}")
+    print("Enter new values (leave blank to keep current value):")
+    with open(file_path, "r") as f:
+        lines = f.readlines()
+    edited_lines = []
+    for line in lines:
+        if "=" in line:
+            key, value = line.strip().split("=", 1)
+            new_value = input(f"{key} [{value}]: ").strip()
+            if new_value:
+                line = f"{key}={new_value}\n"
+        edited_lines.append(line)
+    try:
+        with open(file_path, "w") as f:
+            f.writelines(edited_lines)
+        print("File edited successfully.")
+    except PermissionError:
+        print("Permission denied. Please run the script with administrative privileges (e.g., using sudo).")
+
+def create_desktop_file():
+    print("Creating a new .desktop file:")
+    template = """DESKTOP_FILE=[Desktop Entry]
+Name=
+Comment=
+Exec=$APP_DIR %U
+Icon=
+Terminal=false
+Type=Application
+Encoding=UTF-8
+Categories=;
+Name[en_US]="""
+    file_name = input("Enter the file name (without extension): ").strip() + ".desktop"
+    file_path = os.path.join(DESKTOP_DIR, file_name)
+    if os.path.exists(file_path):
+        print("File already exists.")
+        return
+    try:
+        with open(file_path, "w") as f:
+            f.write(template)
+        print(f"File {file_name} created successfully.")
+        choice = input("Do you want to edit the file now? (y/n): ")
+        if choice.lower() == "y":
+            edit_desktop_file(file_name)
+    except PermissionError:
+        print("Permission denied. Please run the script with administrative privileges (e.g., using sudo).")
+
+def remove_desktop_file(file_name):
+    file_path = os.path.join(DESKTOP_DIR, file_name)
+    if not os.path.exists(file_path):
+        print(f"File {file_name} does not exist.")
+        return
+    try:
+        os.remove(file_path)
+        print(f"File {file_name} removed successfully.")
+    except PermissionError:
+        print("Permission denied. Please run the script with administrative privileges (e.g., using sudo).")
+
+def main():
+    if not os.geteuid() == 0:
+        print("Please run the script with administrative privileges (e.g., using sudo).")
+        sys.exit(1)
+
+    while True:
+        print("Options:")
+        print("1. List .desktop files")
+        print("2. Edit a .desktop file")
+        print("3. Create a new .desktop file")
+        print("4. Remove a .desktop file")
+        print("5. Exit")
+        choice = input("Enter your choice (1-5): ")
+        if choice == "1":
+            os.system('clear')
+            keyword = input("Enter a keyword to filter files (leave blank for all files): ")
+            list_desktop_files(keyword)
+        elif choice == "2":
+            os.system('clear')
+            keyword = input("Enter a keyword to filter files (leave blank for all files): ")
+            list_desktop_files(keyword)
+            file_number = int(input("Enter the file number to edit: "))
+            filtered_files = [f for f in os.listdir(DESKTOP_DIR) if f.endswith(".desktop")]
+            filtered_files = [f for f in filtered_files if not keyword or keyword.lower() in f.lower()]
+            filtered_files.sort()
+            if 1 <= file_number <= len(filtered_files):
+                file_name = filtered_files[file_number - 1]
+                edit_desktop_file(file_name)
+            else:
+                print("Invalid file number.")
+        elif choice == "3":
+            os.system('clear')
+            create_desktop_file()
+        elif choice == "4":
+            os.system('clear')
+            keyword = input("Enter a keyword to filter files (leave blank for all files): ")
+            list_desktop_files(keyword)
+            file_number = int(input("Enter the file number to remove: "))
+            filtered_files = [f for f in os.listdir(DESKTOP_DIR) if f.endswith(".desktop")]
+            filtered_files = [f for f in filtered_files if not keyword or keyword.lower() in f.lower()]
+            filtered_files.sort()
+            if 1 <= file_number <= len(filtered_files):
+                file_name = filtered_files[file_number - 1]
+                remove_desktop_file(file_name)
+            else:
+                print("Invalid file number.")
+        elif choice == "5":
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
